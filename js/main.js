68ca53217b5186180c018aef950c094326f8eccd
Ian Goode <ian.goode@axonista.com>
2015-07-28 15:18:59 +0100

    Reorganise javascript

---
+++ js/main.js
@@ -0,0 +1,45 @@
+
+// Nav Smooth Scroll to Section //
+$('.nav a').click(function() {
+	if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
+	&& location.hostname == this.hostname) {
+
+				var $target = $(this.hash);
+				$target = $target.length && $target
+				|| $('[name=' + this.hash.slice(1) +']');
+				if ($target.length) {
+						var targetOffset = $target.offset().top;
+
+			$('html,body')
+			.animate({scrollTop: targetOffset}, 1000);
+				return false;
+			}
+	}
+});
+
+// Nav Background on Scroll //
+var scroll_start = 0;
+var startchange = $('.nav');
+var offset = startchange.offset();
+$(document).scroll(function() {
+    scroll_start = $(this).scrollTop();
+    if(scroll_start > offset.top) {
+        $('.nav').css({ 'background': 'rgba(36, 42, 53, 0.97)',});
+    } else {
+        $('.nav').css('background-color', 'transparent');
+    }
+});
+
+// Interaction Slider //
+$('.tabs-content div').hide();
+$('.tabs-content div:first').show();
+$('.tabs-nav li:first').addClass('tab-active');
+
+// Change tab class and display content
+$('.tabs-nav a').on('click', function(event){
+  event.preventDefault();
+  $('.tabs-nav li').removeClass('tab-active');
+  $(this).parent().addClass('tab-active');
+  $('.tabs-content div').hide();
+  $($(this).attr('href')).show();
+});
