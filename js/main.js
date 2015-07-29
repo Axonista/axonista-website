cb4fb0417f37ba85ebdd4c7eb8a5ba83744c0c47
Ian Goode <ian.goode@axonista.com>
2015-07-29 11:08:31 +0100

    Remove return false from js

--- js/main.js
+++ js/main.js
@@ -3,17 +3,11 @@
 $('.nav a').click(function() {
 	if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
 	&& location.hostname == this.hostname) {
-
-				var $target = $(this.hash);
-				$target = $target.length && $target
-				|| $('[name=' + this.hash.slice(1) +']');
-				if ($target.length) {
-						var targetOffset = $target.offset().top;
-
-			$('html,body')
-			.animate({scrollTop: targetOffset}, 1000);
-				return false;
-			}
+		var $target = $(this.hash);
+		$target = $target.length && $target || $('[name=' + this.hash.slice(1) +']');
+		if ($target.length) {
+            $('html,body').animate({scrollTop: $target.offset().top}, 500);
+		}
 	}
 });
 
