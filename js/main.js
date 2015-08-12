bef9d6124e6506ef7d3f3354472a97a98d772710
Ian Goode <ian.goode@axonista.com>
2015-08-12 16:19:11 +0100

    Remove ink effect

--- js/main.js
+++ js/main.js
@@ -60,26 +60,3 @@ $(".illustrations .segmented-control input").on("change", function() {
     $parent.find('img:not(".' + image_id + '")').hide();
     $parent.find('img.' + image_id).show();
 });
-
-var element, circle, d, x, y;
-$(".segmented-control label").click(function(e){
-    element = $(this);
-
-    if(element.find(".circle").length == 0) {
-        element.prepend("<span class='circle'></span>");
-    }
-
-    circle = element.find(".circle");
-    circle.removeClass("animate");
-    circle.css({height: 100, width: 100});
-
-    if(!circle.height() && !circle.width()) {
-        d = Math.max(element.outerWidth(), element.outerHeight());
-        circle.css({height: d, width: d});
-    }
-
-    x = e.pageX - element.offset().left - circle.width()/2;
-    y = e.pageY - element.offset().top - circle.height()/2;
-
-    circle.css({top: y+'px', left: x+'px'}).addClass("animate");
-})
